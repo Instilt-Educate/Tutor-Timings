b5382d2a24569f1cadaccf6cb6d67d3ff1e409c3
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-09-02 19:33:30 +0530

    fix error

--- src/fillData.js
+++ src/fillData.js
@@ -36,7 +36,7 @@ async function makePage(){
             },
             {
               name: "11:00 AM - 12:00 PM",
-            }
+            },
             {
               name: "12:00 PM - 1:00 PM",
             }
