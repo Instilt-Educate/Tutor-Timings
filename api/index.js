156aa1286bdb8bcb2d08c7167a15429a040e84bf
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-09-10 04:14:10 -0400

    update path.

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join('../public')));
+app.use(express.static(path.join('./public')));
 
 const notion = new Client({ 
     auth: process.env.API_KEY,
