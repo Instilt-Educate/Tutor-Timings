99a8b95d9aad31353aae416d569307458d821bb8
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-09-10 04:19:01 -0400

    file path fix

--- api/index.js
+++ api/index.js
@@ -11,7 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join('./public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 const notion = new Client({ 
     auth: process.env.API_KEY,
