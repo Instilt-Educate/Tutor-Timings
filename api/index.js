8c8e900f2a9333fc3c5ecafc6daa1168802e1b37
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-09-24 14:27:04 -0400

    update cors

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../public')));
