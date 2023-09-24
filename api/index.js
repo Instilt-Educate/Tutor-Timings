4b9535d9f573b0f7ea7303278be8f1f56ef7a421
Espiobest <59823894+Espiobest@users.noreply.github.com>
2023-09-24 11:32:25 -0400

    update page if email already exists

--- api/index.js
+++ api/index.js
@@ -22,6 +22,70 @@ const notion = new Client({
     auth: process.env.API_KEY,
 });
 
+
+app.post('/getRecords', async (req, res) => {
+  const DATABASE_ID = process.env.DATABASE_ID;
+  let allRecords = [];
+  let nextPageToken = undefined;
+  try {
+      do {
+        const response = await notion.databases.query({
+          database_id: DATABASE_ID,
+          start_cursor: nextPageToken,
+          // filter by status Active, Unresponsive, Joined
+          filter: {
+            or: [
+              {
+                property: "Status", 
+                status: {
+                  equals: "Active" 
+                }
+              },
+              {
+                property: "Status", 
+                status: {
+                  equals: "Unresponsive" 
+                }
+              },
+              // {
+              //   property: "Status", 
+              //   status: {
+              //     equals: "Joined"
+              //   }
+              // },
+              
+            ],
+          },
+        });
+  
+        allRecords.push(...response.results);
+  
+        nextPageToken = response.next_cursor;
+      } while (nextPageToken);
+
+      // remove empty records
+      allRecords = allRecords.filter(record => record.properties.Name.title[0]?.plain_text !== undefined);
+      const formattedRecords = allRecords.map(record => ({
+        id: record.properties.ID.unique_id.number,
+        name: record.properties.Name.title[0]?.plain_text || '',
+        email: record.properties.Email.email || '',
+        position: record.properties.Position.multi_select.length > 0 ? record.properties.Position.multi_select[0].name: record.properties.Team.multi_select[0]?.name.replace("Non-Departmental", "Tutor"),
+        team: record.properties.Team.multi_select[0]?.name.replace("Non-Departmental", "Tutor"),
+        location: record.properties.City.select != undefined ? record.properties.City.select.name + ', ' + record.properties.Country.select?.name : record.properties.Country.select?.name || '',
+        image: record.properties.image.url || '',
+      }));
+
+      formattedRecords.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      // console.log(formattedRecords);
+      // console.log("RETURNING NOW");
+      return formattedRecords;
+    } catch (error) {
+      console.error('Error fetching database records:', error);
+      return [];
+    }
+});
+
+
 app.post('/submitTimes', async (req, res) => {
     // get json data from req
     // send to notion
@@ -37,7 +101,43 @@ app.post('/submitTimes', async (req, res) => {
     const FridaySlots = {multi_select: timeData.friday.map((slot) => ({name: slot,})),};
     const SaturdaySlots = {multi_select: timeData.saturday.map((slot) => ({name: slot,})),};
 
-    const response = await notion.pages.create({
+    const filterEmail = timeData.email;
+
+    // Check if a page with the same email already exists
+    const filterResponse = await notion.databases.query({
+      database_id: databaseId,
+      filter: {
+        property: 'Email', 
+        rich_text: {
+          equals: filterEmail,
+        },
+      },
+    });
+
+    if (filterResponse.results.length > 0) {
+      console.log("PAGE EXISTS, UPDATING")
+      // If a page with the same email exists, update it
+      const existingPageId = filterResponse.results[0].id;
+      
+      const updateResponse = await notion.pages.update({
+        page_id: existingPageId,
+        properties: {
+          // Update the properties you want to change
+          Monday: MondaySlots,
+          Tuesday: TuesdaySlots,
+          Wednesday: WednesdaySlots,
+          Thursday: ThursdaySlots,
+          Friday: FridaySlots,
+          Saturday: SaturdaySlots,
+          Sunday: SundaySlots,
+        },
+      });
+
+      res.send(updateResponse);
+    }
+    else{
+      // create a new page
+      const response = await notion.pages.create({
         parent: {
             database_id: databaseId,
         },
@@ -70,7 +170,9 @@ app.post('/submitTimes', async (req, res) => {
         },
     });
     res.send(response);
+    } 
 });
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
