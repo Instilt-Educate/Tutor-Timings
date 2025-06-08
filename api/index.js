2977c5c6e7b4df75d47478ec136c682175ce2802
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-06-08 13:26:10 -0400

    add max duration

--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const path = require('path');
-const { Client } = require('@notionhq/client');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { Client } from '@notionhq/client';
+import cors from 'cors';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = process.env.PORT || 3000;
 const DATABASE_ID = process.env.DATABASE_ID;
 const app = express();
-const maxDuration = 60;
+export const maxDuration = 60;
 
 app.use(cors());
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../public')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
@@ -226,4 +230,4 @@ app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 });
 
-module.exports = app;
+export default app;
